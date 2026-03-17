1e2a6b77290b01ca418bbe50241f2eb8b5c72e99
hiepphan1411 <22719711.hiep@student.iuh.edu.vn>
2026-03-17 11:34:09 +0700

    init: init project

---
+++ TTS_Website_TuyenSinh_MVC_ASC/Controllers/ThongTinXetTuyenController.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class ThongTinXetTuyenController
+        Inherits Controller
+
+        ' GET: ThongTinXetTuyen
+        Function Index() As ActionResult
+            Return View()
+        End Function
+    End Class
+End Namespace
