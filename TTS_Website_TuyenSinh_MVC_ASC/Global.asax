a6eafd040a67bce9b74dfcb30d1a323980e817df
hiepphan1411 <22719711.hiep@student.iuh.edu.vn>
2026-03-17 11:56:29 +0700

    feat: add _layout

--- TTS_Website_TuyenSinh_MVC_ASC/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.vb" Inherits="TTS_Website_TuyenSinh_MVC_ASC.MvcApplication" Language="VB" %>
---
+++ WebApplicationTest3/Controllers/ThongTinXetTuyenController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationTest3.Controllers
+{
+    public class ThongTinXetTuyenController : Controller
+    {
+        // GET: ThongTinXetTuyen
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
